880dae7e8a01a038b044dfb300fe53846c3dd7fa
Tiago de Oliveira <tiago.zdo@gmail.com>
2023-11-21 13:01:29 -0300

    6- Creación del model Posts  -- Importación y definición de los models User y Posts en db.js

--- src/db.js
+++ src/db.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 const { Sequelize, DataTypes } = require("sequelize");
 
+// importacion de los modelos
+const UsersModel = require('./models/UsersModel');
+const PostModel = require('./models/PostsModel');
+
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 
@@ -9,6 +13,9 @@ const sequelize = new Sequelize(
     { logging: false }
 );
 
+// definicion de modelos
+UsersModel(sequelize);
+PostModel(sequelize);
 
 module.exports = {
     conn: sequelize
